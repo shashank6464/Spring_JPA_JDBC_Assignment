bc3e30c145962314ef852f9fcd97dceca96e05f3
1034517 <shashank.kumar@blueyonder.com>
2022-10-02 21:02:53 +0530

    Spring JPA Assignment with One to one Relation including JDBC queries

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@ INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_
 INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(2, 'Mohan','Yadav','mohan@gmail.com','mohanxyz', null);
 INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(3, 'Shin','Jake','shin07@gmail.com','shin007', null);
 INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(4, 'Chan','Miko','chan123@gmail.com','chan1234', null);
-INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(4, 'John','Ramos','john01@gmail.com','john456', null);
-INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(4, 'Raj','Singh','rajsingh@gmail.com','singh123', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(5, 'John','Ramos','john01@gmail.com','john456', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(6, 'Raj','Singh','rajsingh@gmail.com','singh123', null);
 
 
 
