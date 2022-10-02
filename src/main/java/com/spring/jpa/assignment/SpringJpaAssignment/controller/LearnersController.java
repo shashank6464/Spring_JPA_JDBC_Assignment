09be4e5cae1938d00747df37112416b34fcd5ed2
1034517 <shashank.kumar@blueyonder.com>
2022-10-02 20:54:17 +0530

    Spring JPA Assignment with One to one Relation including JDBC queries

--- src/main/java/com/spring/jpa/assignment/SpringJpaAssignment/controller/LearnersController.java
+++ src/main/java/com/spring/jpa/assignment/SpringJpaAssignment/controller/LearnersController.java
@@ -66,7 +66,7 @@ public class LearnersController {
     }
 
 
-    // Additional Queries -->
+    // ------- Additional Queries -->
 
     @RequestMapping("/findByEmailAddressAndLastname")
     public List<Learners> findByEmailAddressAndLastname(@RequestBody Map<String, Object> fields) {
